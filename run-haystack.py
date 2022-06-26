0e87c8c7ef6e8dc8ea4912a47f549f90876eff32
dmigo <d.f.goryunov@gmail.com>
2022-06-26 23:26:22 +0200

    Download the data

--- run-haystack.py
+++ run-haystack.py
@@ -2,26 +2,42 @@ import logging
 import os
 from pathlib import Path
 from haystack import Document, Pipeline, Answer
+from haystack.utils import fetch_archive_from_http
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-DATA_PATH = Path('./data/wiki_gameofthrones_txt')
+DATA_LOCAL_PATH = Path('./data/wiki_gameofthrones_txt')
+DATA_GLOBAL_PATH =  "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip"
 CONFIG_PATH = Path('./sparse.yaml')
+
+def download_data():
+    doc_dir = DATA_LOCAL_PATH
+    s3_url = DATA_GLOBAL_PATH
+    fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
+
+def get_file_paths():
+    if not DATA_LOCAL_PATH.exists():
+        logger.info("Downloading data from S3")
+        download_data()
+    return [f.path for f in os.scandir(DATA_LOCAL_PATH)]
 
 
 if __name__ == "__main__":
-    logger.info("Starting...")
+    logger.info("Starting")
     indexing_pipeline = Pipeline.load_from_yaml(CONFIG_PATH, "indexing")
     query_pipelilne = Pipeline.load_from_yaml(CONFIG_PATH, "query")
-    file_paths = [f.path for f in os.scandir(DATA_PATH)]
+    file_paths = get_file_paths()
 
-    logger.info("Indexing...")
+    logger.info("Indexing")
     indexing_pipeline.run(
         file_paths=file_paths
     )
-    logger.info("Asking...")
-    result = query_pipelilne.run("Who is the father of Jon Snow?")
+    logger.info("Asking questions")
+    result = query_pipelilne.run("Who plays Arya Stark?")
 
-    logger.info(f"The result is ready and it is: {result}")
+    logger.info("The result is ready and it is:")
+    for answer in result["documents"]:
+        print(answer.content)    
