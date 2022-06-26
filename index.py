f80ed875b085de7634b187e7971a6f6f7656e3d3
dmigo <d.f.goryunov@gmail.com>
2022-06-27 00:03:33 +0200

    Simplify the code

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import logging
 import os
 from pathlib import Path
-from haystack import Document, Pipeline, Answer
+from typing import List
+from haystack import Document, Pipeline
 from haystack.utils import fetch_archive_from_http
 
 
@@ -14,23 +15,18 @@ DATA_GLOBAL_PATH = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/dat
 CONFIG_PATH = Path("./config/dense.yaml")
 
 
-def download_data():
-    doc_dir = DATA_LOCAL_PATH
-    s3_url = DATA_GLOBAL_PATH
-    fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
-
-
-def get_file_paths():
+def get_file_paths(local_path: Path, s3_url: str) -> List[Path]:
     if not DATA_LOCAL_PATH.exists():
         logger.info("Downloading data from S3")
-        download_data()
+        fetch_archive_from_http(url=s3_url, output_dir=local_path)
+
     return [f.path for f in os.scandir(DATA_LOCAL_PATH)]
 
 
 if __name__ == "__main__":
     logger.info("Starting")
     indexing_pipeline = Pipeline.load_from_yaml(CONFIG_PATH, "indexing")
-    file_paths = get_file_paths()
+    file_paths = get_file_paths(DATA_LOCAL_PATH, DATA_GLOBAL_PATH)
 
     logger.info("Indexing")
     indexing_pipeline.run(file_paths=file_paths)
