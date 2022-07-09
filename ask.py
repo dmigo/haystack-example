1ed6d691f9b70c985ce1d55c2125587d661d552e
dmigo <d.f.goryunov@gmail.com>
2022-07-09 21:59:02 +0200

    First attempt

--- ask.py
+++ ask.py
@@ -1,14 +1,45 @@
 import sys
 import logging
 from pathlib import Path
-from typing import List
+from typing import Any, Dict, List
 from haystack import Answer, Pipeline
+import ray
+from ray import serve
+import yaml
+from starlette.requests import Request
+import asyncio
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-CONFIG_PATH = Path("./config/dense.yaml")
+CONFIG_PATH = Path("./config/sparse.yaml")
+
+
+@serve.deployment
+class QueryPipeline:
+    def __init__(self, config: Dict[str, Any]):
+        self.query_pipelilne = Pipeline.load_from_config(config, "query")
+
+    async def __call__(self, request: Request):
+        body = await request.json()
+        question = body.get("question", "Who plays Arya Stark?")
+        result = self.query_pipelilne.run(question)
+        return result
+
+
+def spin_up():
+    with open(CONFIG_PATH, "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            with ray.init(address="auto", namespace="default"):
+                serve.start(detached=True)
+                QueryPipeline.options(name="game-of-thrones-q").deploy(config)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+# def ask():
 
 
 if __name__ == "__main__":
@@ -16,14 +47,13 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         question = sys.argv[1]
 
-    logger.info("Starting")
-    query_pipelilne = Pipeline.load_from_yaml(CONFIG_PATH, "query")
+    spin_up()
 
-    logger.info(f'Asking your question: "{question}"')
-    result = query_pipelilne.run(question)
-    documents: List[Answer] = result["answers"]
+    # logger.info(f'Asking your question: "{question}"')
+    # result =  ask(question)
+    # documents: List[Answer] = result["answers"]
 
-    logger.info("The result is ready and it is:")
-    for answer in documents:
-        print(f"The answer is: '{answer.answer}' with confidence {answer.score}")
-        print(f"The context for the answer is: {answer.context}")
+    # logger.info("The result is ready and it is:")
+    # for answer in documents:
+    #     print(f"The answer is: '{answer.answer}' with confidence {answer.score}")
+    #     print(f"The context for the answer is: {answer.context}")
--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import ray
 from pathlib import Path
-from typing import List
+from typing import Any, Dict, List
+import yaml
 
 import s3fs
 from haystack import Pipeline
@@ -13,7 +15,7 @@ logger.setLevel(logging.INFO)
 
 DATA_LOCAL_PATH = Path("./data/wiki_gameofthrones_txt")
 DATA_GLOBAL_PATH = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip"
-CONFIG_PATH = Path("./config/dense.yaml")
+CONFIG_PATH = Path("./config/sparse.yaml")
 
 
 def get_file_paths(local_path: Path, s3_url: str) -> List[Path]:
@@ -25,11 +27,26 @@ def get_file_paths(local_path: Path, s3_url: str) -> List[Path]:
     return [Path(f.path) for f in os.scandir(local_path)]
 
 
-if __name__ == "__main__":
+@ray.remote
+def run_indexing(config: Dict[str, Any]):
     logger.info("Starting")
     indexing_pipeline = Pipeline.load_from_yaml(CONFIG_PATH, "indexing")
+    Pipeline.load_from_config(config, "indexing")
     file_paths = get_file_paths(DATA_LOCAL_PATH, DATA_GLOBAL_PATH)
 
     logger.info("Indexing")
-    indexing_pipeline.run(file_paths=file_paths)
+    output = indexing_pipeline.run(file_paths=file_paths)
     logger.info("Indexed successfully")
+    return output
+
+
+if __name__ == "__main__":
+    with open(CONFIG_PATH, "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            with ray.init(address="auto", namespace="default") as ray_client:
+                future = run_indexing.remote(config)
+                output = ray_client.get(future)
+                print(output)
+        except yaml.YAMLError as exc:
+            print(exc)
